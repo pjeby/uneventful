a77bf6d601c3d4445b1bee5a2ad42d1b8485024e
PJ <pje@telecommunity.com>
2023-12-25 23:32:55 -0500

    Set typedoc up w/CSS + markdown pages support

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,25 +1,30 @@
 /** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
     entryPoints: ["./src/mod.ts"],
-    out: "docs/ref",
+    customCss: ["./typedoc/custom.css"],
     categorizeByGroup: false,
     categoryOrder: [
         "Flows",
-        "Signals",
         "Resource Management",
+        "Jobs and Scheduling",
+        "Signals",
+        "Stream Consumers",
         "Stream Producers",
         "Stream Operators",
-        "Stream Consumers",
         "*",
         "Errors",
         "Other",
     ],
     hideGenerator: true,
     excludeInternal: true,
+    excludePrivate: true,
+    excludeProtected: true,
+    excludeReferences: true,
     hideParameterTypesInTitle: false,
     navigation: {
         includeCategories: true,
     },
+    options: "package.json", // workaround for https://github.com/KnodesCommunity/typedoc-plugins/issues/525
     sort: [
         "alphabetical"
     ],
